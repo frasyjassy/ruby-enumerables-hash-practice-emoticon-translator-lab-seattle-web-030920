48f66c248f32f7a6fd842ab2820210b0e5aa7e43
JC Ramirez <jsmnramirez25@gmail.com>
2020-04-20 15:54:42 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,5 +1,28 @@
 # require modules here
 
+require 'yaml'
+require 'pry'
+
+def load_library(file_path)
+  file = File.read(file_path)
+  emoticons = YAML.load(file)
+end
+
+def get_japanese_emoticon(file_path, emoticon)
+  emoticons = load_library(file_path)
+  emoji = emoticons.values.find {|faces| faces[0] == emoticon}
+  emoji == nil ? "Sorry, that emoticon was not found" : emoji[1]
+end
+
+def get_english_meaning(file_path, emoticon)
+  emoticons = load_library(file_path)
+  emoji = emoticons.find {|name, faces| faces[1] == emoticon }
+  emoji == nil ? "Sorry, that emoticon was not found" : emoji[0]
+end
+
+
+
+=begin
 def load_library
   # code goes here
 end
@@ -11,3 +34,6 @@ end
 def get_english_meaning
   # code goes here
 end
+
+=end
+
